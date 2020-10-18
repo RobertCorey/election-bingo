763c74d3b4895de0c96980305f10300d4608994e
Robert Corey <robertbcorey@gmail.com>
2020-10-18 16:58:53 -0400

    add state-id to electoral votes lookup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+import { votes } from "./const/electoral-votes-map";
+import { odds } from "./const/state-odds";
+
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("each state should have a votes associated with it", () => {
+  for (let odd of odds) {
+    expect(votes[odd.stateId]).toBeGreaterThan(0);
+  }
 });
---
+++ src/const/state-odds.js
@@ -0,0 +1,282 @@
+export const odds = [
+  {
+    stateId: "Wyoming",
+    trumpChance: ".99845",
+    bidenChance: ".00155",
+  },
+  {
+    stateId: "Wisconsin",
+    trumpChance: ".11455",
+    bidenChance: ".88545",
+  },
+  {
+    stateId: "West Virginia",
+    trumpChance: ".986225",
+    bidenChance: ".013775",
+  },
+  {
+    stateId: "Washington",
+    trumpChance: ".0073",
+    bidenChance: ".9927",
+  },
+  {
+    stateId: "Virginia",
+    trumpChance: ".0092",
+    bidenChance: ".9908",
+  },
+  {
+    stateId: "Vermont",
+    trumpChance: ".007625",
+    bidenChance: ".992375",
+  },
+  {
+    stateId: "Utah",
+    trumpChance: ".958875",
+    bidenChance: ".041125",
+  },
+  {
+    stateId: "Texas",
+    trumpChance: ".686675",
+    bidenChance: ".313325",
+  },
+  {
+    stateId: "Tennessee",
+    trumpChance: ".965775",
+    bidenChance: ".034225",
+  },
+  {
+    stateId: "South Dakota",
+    trumpChance: ".966425",
+    bidenChance: ".033575",
+  },
+  {
+    stateId: "South Carolina",
+    trumpChance: ".88985",
+    bidenChance: ".11015",
+  },
+  {
+    stateId: "Rhode Island",
+    trumpChance: ".0011",
+    bidenChance: ".9989",
+  },
+  {
+    stateId: "Pennsylvania",
+    trumpChance: ".12625",
+    bidenChance: ".87375",
+  },
+  {
+    stateId: "Oregon",
+    trumpChance: ".0255",
+    bidenChance: ".9745",
+  },
+  {
+    stateId: "Oklahoma",
+    trumpChance: ".9925",
+    bidenChance: ".0075",
+  },
+  {
+    stateId: "Ohio",
+    trumpChance: ".512375",
+    bidenChance: ".487625",
+  },
+  {
+    stateId: "North Dakota",
+    trumpChance: ".989425",
+    bidenChance: ".010575",
+  },
+  {
+    stateId: "North Carolina",
+    trumpChance: ".330125",
+    bidenChance: ".669875",
+  },
+  {
+    stateId: "New York",
+    trumpChance: ".0002",
+    bidenChance: ".9998",
+  },
+  {
+    stateId: "New Mexico",
+    trumpChance: ".02865",
+    bidenChance: ".97135",
+  },
+  {
+    stateId: "New Jersey",
+    trumpChance: ".01335",
+    bidenChance: ".98665",
+  },
+  {
+    stateId: "New Hampshire",
+    trumpChance: ".137025",
+    bidenChance: ".862975",
+  },
+  {
+    stateId: "Nevada",
+    trumpChance: ".131625",
+    bidenChance: ".868375",
+  },
+  {
+    stateId: "Nebraska",
+    trumpChance: ".9893",
+    bidenChance: ".0107",
+  },
+  {
+    stateId: "NE-3",
+    trumpChance: ".99975",
+    bidenChance: ".00025",
+  },
+  {
+    stateId: "NE-2",
+    trumpChance: ".215175",
+    bidenChance: ".784825",
+  },
+  {
+    stateId: "NE-1",
+    trumpChance: ".9439",
+    bidenChance: ".0561",
+  },
+  {
+    stateId: "Montana",
+    trumpChance: ".892575",
+    bidenChance: ".107425",
+  },
+  {
+    stateId: "Missouri",
+    trumpChance: ".907125",
+    bidenChance: ".092875",
+  },
+  {
+    stateId: "Mississippi",
+    trumpChance: ".8822",
+    bidenChance: ".1178",
+  },
+  {
+    stateId: "Minnesota",
+    trumpChance: ".07655",
+    bidenChance: ".92345",
+  },
+  {
+    stateId: "Michigan",
+    trumpChance: ".088225",
+    bidenChance: ".911775",
+  },
+  {
+    stateId: "Massachusetts",
+    trumpChance: ".000625",
+    bidenChance: ".999375",
+  },
+  {
+    stateId: "Maryland",
+    trumpChance: ".000475",
+    bidenChance: ".999525",
+  },
+  {
+    stateId: "Maine",
+    trumpChance: ".101425",
+    bidenChance: ".898575",
+  },
+  {
+    stateId: "ME-2",
+    trumpChance: ".482725",
+    bidenChance: ".517275",
+  },
+  {
+    stateId: "ME-1",
+    trumpChance: ".022825",
+    bidenChance: ".977175",
+  },
+  {
+    stateId: "Louisiana",
+    trumpChance: ".9348",
+    bidenChance: ".0652",
+  },
+  {
+    stateId: "Kentucky",
+    trumpChance: ".9851",
+    bidenChance: ".0149",
+  },
+  {
+    stateId: "Kansas",
+    trumpChance: ".926125",
+    bidenChance: ".073875",
+  },
+  {
+    stateId: "Iowa",
+    trumpChance: ".581825",
+    bidenChance: ".418175",
+  },
+  {
+    stateId: "Indiana",
+    trumpChance: ".939175",
+    bidenChance: ".060825",
+  },
+  {
+    stateId: "Illinois",
+    trumpChance: ".0007",
+    bidenChance: ".9993",
+  },
+  {
+    stateId: "Idaho",
+    trumpChance: ".994525",
+    bidenChance: ".005475",
+  },
+  {
+    stateId: "Hawaii",
+    trumpChance: ".00695",
+    bidenChance: ".99305",
+  },
+  {
+    stateId: "Georgia",
+    trumpChance: ".492725",
+    bidenChance: ".507275",
+  },
+  {
+    stateId: "Florida",
+    trumpChance: ".28845",
+    bidenChance: ".71155",
+  },
+  {
+    stateId: "District of Columbia",
+    trumpChance: "0",
+    bidenChance: "1",
+  },
+  {
+    stateId: "Delaware",
+    trumpChance: ".00005",
+    bidenChance: ".99995",
+  },
+  {
+    stateId: "Connecticut",
+    trumpChance: ".001925",
+    bidenChance: ".998075",
+  },
+  {
+    stateId: "Colorado",
+    trumpChance: ".0532",
+    bidenChance: ".9468",
+  },
+  {
+    stateId: "California",
+    trumpChance: ".00125",
+    bidenChance: ".99875",
+  },
+  {
+    stateId: "Arkansas",
+    trumpChance: ".975625",
+    bidenChance: ".024375",
+  },
+  {
+    stateId: "Arizona",
+    trumpChance: ".319625",
+    bidenChance: ".680375",
+  },
+  {
+    stateId: "Alaska",
+    trumpChance: ".778675",
+    bidenChance: ".221325",
+  },
+  {
+    stateId: "Alabama",
+    trumpChance: ".97785",
+    bidenChance: ".02215",
+  },
+];
