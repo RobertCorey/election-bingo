c4dbb887052db59efd08cf02f5944cffb27faf59
Tom Corey <tomcorey26@my.uri.edu>
2020-10-18 20:22:26 -0400

    Data parser most recent dates (#3)
    * created parser
    
    * acciedntly deleted parser lol recommit
    
    * only does by most recent date
    
    * data json

--- parser.py
+++ parser.py
@@ -1,21 +1,23 @@
 import csv
 import json
 
-def toJson(path):
+# date format mm/dd/yyyy
+def toJson(path,mostRecentDate):
   with open(path, mode='r') as csv_file:
       csv_reader = csv.DictReader(csv_file)
       arr = []
       for row in csv_reader:
-          obj = {
-            "stateId":row["state"], 
-            "trumpChance":row['winstate_inc'],
-            "bidenChance":row['winstate_chal']
-          }
-          print(obj)
-          arr.append(obj)
+          if row["modeldate"] == mostRecentDate:
+            obj = {
+              "stateId":row["state"], 
+              "trumpChance":row['winstate_inc'],
+              "bidenChance":row['winstate_chal']
+            }
+            print(obj)
+            arr.append(obj)
   return arr
 
-data = toJson('./election-forecasts-2020/presidential_state_toplines_2020.csv')
+data = toJson('./election-forecasts-2020/presidential_state_toplines_2020.csv','10/17/2020')
 
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
